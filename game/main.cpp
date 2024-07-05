326f8af2b3e4502c95a8bb845ebec9c90ed701e8
Dolphcode <bastikulit@gmail.com>
2024-07-04 20:41:48 -0400

    Create window and import GLAD

--- game/main.cpp
+++ game/main.cpp
@@ -1,5 +1,39 @@
+#include <glad/glad.h> // NOTE: Include GLAD first
+#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
 	std::cout << "Hello World!\n";
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window{};
+	window = glfwCreateWindow(800, 600, "Block Game", NULL, NULL);
+	if (!window) {
+		std::cout << "Failed to create window\n";
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to load GLAD";
+		glfwTerminate();
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	while (!glfwWindowShouldClose(window)) {
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	
+	glfwTerminate();
+	return 0;
 }
